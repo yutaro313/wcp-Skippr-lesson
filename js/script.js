4b24646352e0db2af3764b3b05b8c3536326d84a
yutaro313 <yuutaro.0313.soccer@gmail.com>
2021-05-27 15:40:41 +0900

    [Start]Skippr-lesson

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+  transition : 'fade',
+  speed : 1000,
+  easing : 'easeOutQuart',
+  nacType : 'block',
+  childrenElementType : 'div',
+  arrows : true,
+  autoPlay : false,
+  autoPlayDuration : 5000,
+  keyboardOnAlways : true,
+  hidePrevious : false
+});
